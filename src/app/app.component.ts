d0bbcd46b525909c5a156e6f02f1bf63f21a4593
Aditya Wirya  Maulana <aditya.maulana@salt.co.id>
2023-03-22 00:40:30 +0700

    initiate form builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,130 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { NotificationService, ObjectUtils } from '@asoftwareworld/form-builder/form-control/core';
+import { AswJsonPreviewDialog } from '@asoftwareworld/form-builder/form-control/json-preview-dialog';
+import { SIMPLE_CONTROLS, CHOICE_CONTROLS, DATE_AND_GPS_CONTROLS, DIGITAL_CONTROLS, OTHERS_CONTROLS } from './form-builder/default-controls';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'form-builder';
+export class AppComponent implements OnInit {
+
+  @Input() uploadData: any[] = [];
+  @Input() isShowPreviewButton = true;
+  @Input() isShowJsonDataButton = true;
+  @Input() isShowPublishButton = true;
+
+  @Output() buttonClick = new EventEmitter<any[]>();
+  @Output() previewClick = new EventEmitter<any[]>();
+  @Output() publishClick = new EventEmitter<any[]>();
+
+  constructor(
+    private notificationService: NotificationService,
+    public dialog: MatDialog,
+  ) { }
+  ngOnInit(): void {
+    this.simpleControls = SIMPLE_CONTROLS;
+    this.choiceControls = CHOICE_CONTROLS;
+    this.dateAndGpsControls = DATE_AND_GPS_CONTROLS;
+    this.digitalControls = DIGITAL_CONTROLS;
+    this.othersControls = OTHERS_CONTROLS;
+  }
+  step = 0;
+
+  title = 'ASW Form Builder Demo';
+  simpleControls: any[] = [];
+  jsonData: any[] = [];
+  dateAndGpsControls: any[] = [];
+  choiceControls: any[] = [];
+  digitalControls: any[] = [];
+  othersControls: any[] = [];
+  jsonData1: any[] = [];
+  formContainer: any[] = [];
+
+  // Publish Template
+  saveJsonData(data: any) {
+    //.... 
+    console.log(data);
+    // do something
+  }
+
+  //Preview Template
+  // previewTemplate(data: any) {
+  //   this.jsonData = data;
+  // }
+
+  buttonClickV2(data: any): void {
+    console.log(data);
+  }
+
+  drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(JSON.parse(JSON.stringify(event.previousContainer.data)),
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+  onSelectionChange(control: any): void {
+    console.log(control);
+  }
+
+  updatedControl(data: any): void {
+    this.formContainer.splice(data.index, 1, data.control);
+  }
+  duplicateControl(control: any): void {
+    const duplicatedControl = JSON.parse(JSON.stringify(control));
+    if (duplicatedControl.controlType === 'fileupload') {
+      duplicatedControl.value = [];
+    } else if (duplicatedControl.controlType !== 'qr-code') {
+      duplicatedControl.value = duplicatedControl.controlType === 'slide-toggle' ? false : '';
+    }
+    this.formContainer.push(duplicatedControl);
+  }
+
+  deleteControl(index: any): void {
+    this.formContainer.splice(index, 1);
+  }
+
+  buttonClicked(type: string): void {
+    if (type === 'reset') {
+      this.formContainer = ObjectUtils.resetForm(this.formContainer);
+    } else {
+      const data = ObjectUtils.validateForm(this.formContainer);
+      if (!data.isFormValid) {
+        this.notificationService.openNotification('Please fill in the following required fields.'
+          + ' \n \n' + data.labels.toString(), 'Close');
+      } else {
+        this.buttonClick.emit(this.formContainer);
+      }
+    }
+  }
+
+  setStep(index: number): void {
+    this.step = index;
+  }
+
+  previewTemplate(): void {
+    this.previewClick.emit(this.formContainer);
+  }
+
+  previewJsonData(): void {
+    const dialogRef = this.dialog.open(AswJsonPreviewDialog, {
+      disableClose: true,
+      width: '744px',
+      data: this.formContainer
+    });
+    dialogRef.afterClosed();
+  }
+
+  publishTemplate(): void {
+    this.publishClick.emit(this.formContainer);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AswFormBuilderModule } from '@asoftwareworld/form-builder/form-builder';
+import { AswPreviewTemplateModule } from '@asoftwareworld/form-builder/preview-template';
+import { AswAutocompleteModule } from '@asoftwareworld/form-builder/form-control/autocomplete';
+import { AswButtonModule } from '@asoftwareworld/form-builder/form-control/button';
+import { AswCalculationModule } from '@asoftwareworld/form-builder/form-control/calculation';
+import { AswCheckboxModule } from '@asoftwareworld/form-builder/form-control/checkbox';
+import { AswDatepickerModule } from '@asoftwareworld/form-builder/form-control/datepicker';
+import { AswJsonPreviewDialogModule } from '@asoftwareworld/form-builder/form-control/json-preview-dialog';
+import { AswMultiSelectModule } from '@asoftwareworld/form-builder/form-control/multi-select';
+import { AswNumberModule } from '@asoftwareworld/form-builder/form-control/number';
+import { AswParagraphModule } from '@asoftwareworld/form-builder/form-control/paragraph';
+import { AswRadioButtonModule } from '@asoftwareworld/form-builder/form-control/radio-button';
+import { AswSelectModule } from '@asoftwareworld/form-builder/form-control/select';
+import { AswSlideToggleModule } from '@asoftwareworld/form-builder/form-control/slide-toggle';
+import { AswTextareaModule } from '@asoftwareworld/form-builder/form-control/textarea';
+import { AswTextFieldModule } from '@asoftwareworld/form-builder/form-control/textfield';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +33,29 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AswFormBuilderModule,
+    AswPreviewTemplateModule,
+    AswSlideToggleModule,
+    AswTextareaModule,
+    AswTextFieldModule,
+    AswRadioButtonModule,
+    AswJsonPreviewDialogModule,
+    AswNumberModule,
+    AswCalculationModule,
+    AswAutocompleteModule,
+    AswButtonModule,
+    AswCheckboxModule,
+    AswDatepickerModule,
+    AswMultiSelectModule,
+    AswParagraphModule,
+    AswSelectModule,
+    DragDropModule,
+    ClipboardModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
